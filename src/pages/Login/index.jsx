c01aba94384a461541f06022ed706b0eeeb5309a
LuPeBreak <lfbmrj15@gmail.com>
2022-12-02 21:11:07 -0300

    inicio da estrutura

---
+++ src/pages/Login/index.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+
+function Login() {
+  return (
+    <>
+      <h1>Login</h1>
+      <Link to="/">
+        home
+      </Link>
+    </>
+  );
+}
+
+export default Login;
