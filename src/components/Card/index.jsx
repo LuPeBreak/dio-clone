50b8b21f9e2d6ba3be87f5034b9f29e0fb0a4cb4
LuPeBreak <lfbmrj15@gmail.com>
2022-12-04 09:52:25 -0300

    finalizado estrutura statica do card

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { FiThumbsUp } from "react-icons/fi";
 
 import {
   CardContainer,
@@ -11,7 +12,33 @@ import {
 } from "./styles";
 
 function Card() {
-  return <div />;
+  return (
+    <CardContainer>
+      <ImageBackground />
+      <Content>
+        <UserInfo>
+          <UserPicture />
+          <div>
+            <h4>Luis Felipe</h4>
+            <p>Há 8 minutos</p>
+          </div>
+        </UserInfo>
+        <PostInfo>
+          <h4>Projeto para curso de HTML e CSS</h4>
+          <p>
+            Projeto feito no curso de html e css no bootcamp dio do Global
+            avanade...<strong>Saiba Mais</strong>{" "}
+          </p>
+        </PostInfo>
+        <HasInfo>
+          <h4>#HTML #CSS #Javascript</h4>
+          <p>
+            <FiThumbsUp /> 10
+          </p>
+        </HasInfo>
+      </Content>
+    </CardContainer>
+  );
 }
 
 export default Card;
